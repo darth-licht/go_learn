cd27a4a1a45f0518e521d413c60c2c5328737c31
wilfredasomani <devwilfredagyei@gmail.com>
2022-06-29 10:57:42 +0000

    more interface lessons

--- basics/interfaces.go
+++ basics/interfaces.go
@@ -12,11 +12,27 @@ type ConsoleLogger struct{}
 
 // Log function from Logger interface
 // implementation is implicit
-func (logger ConsoleLogger) Log(message string) {
+func (l ConsoleLogger) Log(message string) {
 	fmt.Println(message)
 }
 
-type Add func(a int, b int) int
+// this method comes from the Stringer interface
+// a Stringer can describe itself as a string
+// it is akin to the toString method in java
+// the print functions in fmt look for this method
+func (l ConsoleLogger) String() string {
+	return fmt.Sprint("I am a logger!!")
+}
+
+type MyError struct{}
+
+// this method is from the error interface
+// now MyError is implicitly and (error)
+func (e MyError) Error() string {
+	return fmt.Sprint("error!!")
+}
+
+type Add func(a int, b int) int // function type
 
 func process(adder Add) int {
 	return adder(1, 2)
@@ -32,5 +48,6 @@ func ShowcaseInterfaces() {
 	fmt.Printf("%d\n", process(func(a int, b int) int {
 		return a + b
 	}))
+	fmt.Println(logger.(ConsoleLogger))
 	logger.Log(">>>>End interfaces<<<<")
 }
